7e65895c70562f5c4874ca99e015c4b6ba42ebdb
controllerzz <79202101363@mail.ru>
2024-09-15 18:32:27 +0300

    alembic init

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.openapi.docs import get_swagger_ui_html
-from hotels import router as router_hotels
+from src.api.hotels import router as router_hotels
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
 
 app = FastAPI(docs_url=None)
 app.include_router(router_hotels)
