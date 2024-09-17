b3c29c012e4ac624215c846ab0e82811dfa6d3d0
controllerzz <79202101363@mail.ru>
2024-09-17 03:01:25 +0300

    exercise_10

--- src/api/auth.py
+++ src/api/auth.py
@@ -46,3 +46,10 @@ async def get_me(
         user = await UsersRepository(session).get_one_or_none(id=user_id)
         return user
 
+
+@router.post("/logout")
+async def logout(
+        response: Response,
+):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
